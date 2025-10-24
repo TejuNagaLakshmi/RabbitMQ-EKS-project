c89cbbe333049caf1e3a08548567695da1ba741c
Hima0206 <Himateja0206@gmail.com>
2025-10-24 16:02:17 +0530

    Add availability zones variable and update defaults
    Added availability zones variable and updated CIDR blocks.

--- Infrastructure/Terraform/variables.tf
+++ Infrastructure/Terraform/variables.tf
@@ -3,32 +3,37 @@ variable "region" {
   default = "ca-central-1"
 }
 
+variable "availability_zones" {
+  type    = list(string)
+  default = ["ca-central-1a", "ca-central-1b"]
+}
+
 variable "stack_name" {
   type    = string
-  default = "my-eks-vpc"
+  default = "my-eks-vpc-new"
 }
 
 variable "vpc_cidr" {
   type    = string
-  default = "192.168.0.0/16"
+  default = "10.0.0.0/16"
 }
 
 variable "public_subnet_01_cidr" {
   type    = string
-  default = "192.168.0.0/18"
+  default = "10.0.0.0/18"
 }
 
 variable "public_subnet_02_cidr" {
   type    = string
-  default = "192.168.64.0/18"
+  default = "10.0.64.0/18"
 }
 
 variable "private_subnet_01_cidr" {
   type    = string
-  default = "192.168.128.0/18"
+  default = "10.0.128.0/18"
 }
 
 variable "private_subnet_02_cidr" {
   type    = string
-  default = "192.168.192.0/18"
+  default = "10.0.192.0/18"
 }
