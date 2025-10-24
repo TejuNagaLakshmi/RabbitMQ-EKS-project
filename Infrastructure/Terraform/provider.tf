6b064d3b4c972d60be314f08cf7c1f2a12d5f1c5
Hima0206 <Himateja0206@gmail.com>
2025-10-24 16:01:36 +0530

    Configure Terraform provider settings
    Add required providers and version constraints for Terraform.

--- Infrastructure/Terraform/provider.tf
+++ Infrastructure/Terraform/provider.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"  # Using a stable version of the AWS provider
+    }
+  }
+  required_version = ">= 1.0"
+}
+
 provider "aws" {
   region = var.region
 }
